a5f8ea6e1bcb8e2b63139ec01bc9eea7fade9e1e
nasruddinshafie <mohammadnasruddin0447@gmail.com>
2022-08-01 22:43:14 +0800

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -54,6 +54,8 @@ namespace API
 
             app.UseRouting();
 
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
