a6e8d860d02af1ce0d3e42e8b281bfa209dd198d
nasruddinshafie <mohammadnasruddin0447@gmail.com>
2022-08-09 00:36:59 +0800

    end of section 4

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -4,14 +4,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class UsersController : ControllerBase
+    
+    public class UsersController : BaseApiController
     {
         private readonly DataContext _context;
         
@@ -21,13 +21,14 @@ namespace API.Controllers
            
         }
 
-
+        
         [HttpGet]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
             return await _context.Users.ToListAsync();
         }
 
+        [Authorize]
         //api/users/3
         [HttpGet("{id}")]
           public async Task<ActionResult<AppUser>> GetUser(int id)
---
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Interfaces;
+using API.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddApplicationService(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddScoped<ITokenService ,TokenService>();
+            
+            services.AddDbContext<DataContext>(options => 
+            {
+                options.UseSqlite(config.GetConnectionString("DefaultConnection"));    
+            });
+
+
+            return services;
+        }
+    }
+}
